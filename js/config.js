523142bd5919fdcc659037090ca10c50ef400b38
Piterson Murilo Boscolo <88910148+piterboscolo@users.noreply.github.com>
2026-09-24 21:27:44 -0300

    new

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
 // Credenciais do projeto Supabase OBE 2026
-// ATENÇÃO: a anon key deve ser do MESMO projeto da URL
-// Project ref da sua chave: cuubrwtkmxtihxumlwei
 window.OBE_SUPABASE = {
-  url: "https://cuubrwtkmxtihxumlwei.supabase.co",
+  url: "https://cuubrtwkmxtihxumlwei.supabase.co",
   anonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1dWJydHdrbXh0aWh4dW1sd2VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyODg3MzQsImV4cCI6MjEwNTg2NDczNH0.lUtdq-8FtuCeiiMiLEq8gpOE1n-iFMrDsqxlXrWi-98",
 };
